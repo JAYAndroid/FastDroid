0a5908ee6157266c8452696ccc65ef470426240a
JAYAndroid <380413512@qq.com>
2018-03-15 14:38:32 +0800

    调整StatusBarManager修改背景色方案

--- ui/src/main/java/com/ylz/ehui/ui/manager/StatusBarManager.java
+++ ui/src/main/java/com/ylz/ehui/ui/manager/StatusBarManager.java
@@ -1,11 +1,19 @@
 package com.ylz.ehui.ui.manager;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Build;
-import android.support.annotation.ColorInt;
+import android.support.v4.view.ViewCompat;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
+
+import com.ylz.ehui.base_ui.R;
 
 /**
  * Author: yms
@@ -13,21 +21,200 @@ import android.view.WindowManager;
  * Desc: 状态栏透明化（适配4.4 ，5.0以上）
  */
 public class StatusBarManager {
+    private static final String TAG_FAKE_STATUS_BAR_VIEW = "statusBarView";
+    private static final String TAG_MARGIN_ADDED = "marginAdded";
+//    public static void setStatusBarColor(Activity activity, @ColorInt int color) {
+//        Window window = activity.getWindow();
+//        // Android 5.x 以上
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+//            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+//            window.setStatusBarColor(color);
+//            return;
+//        }
+//
+//        // Android 4.4 — 5.0
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+//            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//        }
+//    }
 
-    public static void setStatusBarColor(Activity activity, @ColorInt int color) {
-        Window window = activity.getWindow();
-        // Android 5.x 以上
+    /**
+     * 透明状态栏
+     *
+     * @param activity
+     * @param hideStatusBarBackground
+     */
+    static void translucentStatusBar(Activity activity, boolean hideStatusBarBackground) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            Window window = activity.getWindow();
+            //添加Flag把状态栏设为可绘制模式
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(color);
+            if (hideStatusBarBackground) {
+                //如果为全透明模式，取消设置Window半透明的Flag
+                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                //设置状态栏为透明
+                window.setStatusBarColor(Color.TRANSPARENT);
+                //设置window的状态栏不可见
+                window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            } else {
+                //如果为半透明模式，添加设置Window半透明的Flag
+                window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                //设置系统状态栏处于可见状态
+                window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+            }
+            //view不根据系统窗口来调整自己的布局
+            ViewGroup mContentView = window.findViewById(Window.ID_ANDROID_CONTENT);
+            View mChildView = mContentView.getChildAt(0);
+            if (mChildView != null) {
+                mChildView.setFitsSystemWindows(false);
+                ViewCompat.requestApplyInsets(mChildView);
+            }
+
             return;
         }
 
-        // Android 4.4 — 5.0
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            Window window = activity.getWindow();
+            //设置Window为透明
             window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+            ViewGroup mContentView = activity.findViewById(Window.ID_ANDROID_CONTENT);
+            View mContentChild = mContentView.getChildAt(0);
+
+            //移除已经存在假状态栏则,并且取消它的Margin间距
+            removeFakeStatusBarViewIfExist(activity);
+            removeMarginTopOfContentChild(mContentChild, getStatusBarHeight(activity));
+            if (mContentChild != null) {
+                //fitsSystemWindow 为 false, 不预留系统栏位置.
+                mContentChild.setFitsSystemWindows(false);
+            }
         }
     }
+
+    /**
+     * 设置状态栏颜色
+     *
+     * @param activity
+     * @param statusColor
+     */
+    public static void setStatusBarColor(Activity activity, int statusColor) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            Window window = activity.getWindow();
+            //设置Window为全透明
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+            ViewGroup mContentView = window.findViewById(Window.ID_ANDROID_CONTENT);
+            //获取父布局
+            View mContentChild = mContentView.getChildAt(0);
+            //获取状态栏高度
+            int statusBarHeight = getStatusBarHeight(activity);
+
+            //如果已经存在假状态栏则移除，防止重复添加
+            removeFakeStatusBarViewIfExist(activity);
+            //添加一个View来作为状态栏的填充
+            addFakeStatusBarView(activity, statusColor, statusBarHeight);
+            //设置子控件到状态栏的间距
+            addMarginTopToContentChild(mContentChild, statusBarHeight);
+            //不预留系统栏位置
+            if (mContentChild != null) {
+                mContentChild.setFitsSystemWindows(false);
+            }
+
+            //如果在Activity中使用了ActionBar则需要再将布局与状态栏的高度跳高一个ActionBar的高度，否则内容会被ActionBar遮挡
+            int action_bar_id = activity.getResources().getIdentifier("action_bar", "id", activity.getPackageName());
+            View view = activity.findViewById(action_bar_id);
+            if (view != null) {
+                TypedValue typedValue = new TypedValue();
+                if (activity.getTheme().resolveAttribute(R.attr.actionBarSize, typedValue, true)) {
+                    int actionBarHeight = TypedValue.complexToDimensionPixelSize(typedValue.data, activity.getResources().getDisplayMetrics());
+                    setContentTopPadding(activity, actionBarHeight);
+                }
+            }
+
+            return;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = activity.getWindow();
+            //取消状态栏透明
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //添加Flag把状态栏设为可绘制模式
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            //设置状态栏颜色
+            window.setStatusBarColor(statusColor);
+            //设置系统状态栏处于可见状态
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+            //让view不根据系统窗口来调整自己的布局
+            ViewGroup mContentView = window.findViewById(Window.ID_ANDROID_CONTENT);
+            View mChildView = mContentView.getChildAt(0);
+            if (mChildView != null) {
+                mChildView.setFitsSystemWindows(false);
+                ViewCompat.requestApplyInsets(mChildView);
+            }
+        }
+    }
+
+    private static void setContentTopPadding(Activity activity, int padding) {
+        ViewGroup mContentView = activity.getWindow().findViewById(Window.ID_ANDROID_CONTENT);
+        mContentView.setPadding(0, padding, 0, 0);
+    }
+
+    private static void removeFakeStatusBarViewIfExist(Activity activity) {
+        Window window = activity.getWindow();
+        ViewGroup mDecorView = (ViewGroup) window.getDecorView();
+
+        View fakeView = mDecorView.findViewWithTag(TAG_FAKE_STATUS_BAR_VIEW);
+        if (fakeView != null) {
+            mDecorView.removeView(fakeView);
+        }
+    }
+
+    private static void removeMarginTopOfContentChild(View mContentChild, int statusBarHeight) {
+        if (mContentChild == null) {
+            return;
+        }
+        if (TAG_MARGIN_ADDED.equals(mContentChild.getTag())) {
+            FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) mContentChild.getLayoutParams();
+            lp.topMargin -= statusBarHeight;
+            mContentChild.setLayoutParams(lp);
+            mContentChild.setTag(null);
+        }
+    }
+
+    private static void addFakeStatusBarView(Activity activity, int statusBarColor, int statusBarHeight) {
+        Window window = activity.getWindow();
+        ViewGroup mDecorView = (ViewGroup) window.getDecorView();
+        View mStatusBarView = new View(activity);
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, statusBarHeight);
+        layoutParams.gravity = Gravity.TOP;
+        mStatusBarView.setLayoutParams(layoutParams);
+        mStatusBarView.setBackgroundColor(statusBarColor);
+        mStatusBarView.setTag(TAG_FAKE_STATUS_BAR_VIEW);
+        mDecorView.addView(mStatusBarView);
+    }
+
+    private static int getStatusBarHeight(Context context) {
+        int result = 0;
+        int resId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resId > 0) {
+            result = context.getResources().getDimensionPixelOffset(resId);
+        }
+
+        return result;
+    }
+
+    private static void addMarginTopToContentChild(View mContentChild, int statusBarHeight) {
+        if (mContentChild == null) {
+            return;
+        }
+
+        if (!TAG_MARGIN_ADDED.equals(mContentChild.getTag())) {
+            FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) mContentChild.getLayoutParams();
+            lp.topMargin += statusBarHeight;
+            mContentChild.setLayoutParams(lp);
+            mContentChild.setTag(TAG_MARGIN_ADDED);
+        }
+    }
+
 }
