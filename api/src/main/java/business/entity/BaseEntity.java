ca9a27852ddf2aaa6d43887d8696ba5435b16c36
JAYAndroid <380413512@qq.com>
2018-04-16 13:55:45 +0800

    抽取baseentity

---
+++ api/src/main/java/business/entity/BaseEntity.java
@@ -0,0 +1,78 @@
+package business.entity;
+
+import java.io.Serializable;
+
+public class BaseEntity<P> implements Serializable {
+    public String encryptType;
+    public String encryptData;
+    public String respCode;
+    public String respMsg;
+    public String sign;
+    public String signType;
+    public String timestamp;
+    private P param;
+
+    public P getParam() {
+        return param;
+    }
+
+    public void setParam(P param) {
+        this.param = param;
+    }
+
+    public String getEncryptType() {
+        return encryptType;
+    }
+
+    public void setEncryptType(String encryptType) {
+        this.encryptType = encryptType;
+    }
+
+    public String getRespCode() {
+        return respCode;
+    }
+
+    public void setRespCode(String respCode) {
+        this.respCode = respCode;
+    }
+
+    public String getRespMsg() {
+        return respMsg;
+    }
+
+    public void setRespMsg(String respMsg) {
+        this.respMsg = respMsg;
+    }
+
+    public String getSignType() {
+        return signType;
+    }
+
+    public void setSignType(String signType) {
+        this.signType = signType;
+    }
+
+    public String getEncryptData() {
+        return encryptData;
+    }
+
+    public void setEncryptData(String encryptData) {
+        this.encryptData = encryptData;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
