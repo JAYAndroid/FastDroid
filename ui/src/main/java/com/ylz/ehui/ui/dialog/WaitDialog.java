fcc3542239eceb9928c29f782334bfdfbb0a41ec
JAYAndroid <380413512@qq.com>
2018-04-16 18:44:59 +0800

    修改basedialog宽高可以配置

--- ui/src/main/java/com/ylz/ehui/ui/dialog/WaitDialog.java
+++ ui/src/main/java/com/ylz/ehui/ui/dialog/WaitDialog.java
@@ -3,12 +3,16 @@ package com.ylz.ehui.ui.dialog;
 import android.view.View;
 
 import com.ylz.ehui.base_ui.R;
+import com.ylz.ehui.utils.SizeUtils;
 
 public class WaitDialog extends BaseDialogFragment {
     @Override
     protected Builder build(Builder builder) {
-        return builder.setView(R.layout.fast_droid_dialog_wait);
+        return builder.setView(R.layout.fast_droid_dialog_wait)
+                .height(SizeUtils.dp2px(120))
+                .width(SizeUtils.dp2px(120));
     }
+
 
     @Override
     protected void doInit(View parent) {
