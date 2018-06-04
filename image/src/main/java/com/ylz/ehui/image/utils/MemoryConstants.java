03fea156acfa032420b69a35a2fcd7a5d0d66802
JAYAndroid <380413512@qq.com>
2018-06-04 16:51:36 +0800

    增加ImageUtils

---
+++ image/src/main/java/com/ylz/ehui/image/utils/MemoryConstants.java
@@ -0,0 +1,18 @@
+package com.ylz.ehui.image.utils;
+
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+public class MemoryConstants {
+    public static final int BYTE = 1;
+    public static final int KB = 1024;
+    public static final int MB = 1048576;
+    public static final int GB = 1073741824;
+
+    @IntDef({BYTE, KB, MB, GB})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Unit {
+    }
+}
